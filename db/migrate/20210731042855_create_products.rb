a8928a7005e4ee2b038502dabaee35476117c5a6
syota0402 <syota.3y5s@gmail.com>
2021-07-31 04:49:43 +0000

    スペルミスを修正

--- db/migrate/20210731042855_create_products.rb
+++ db/migrate/20210731042855_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.intger :price
+      t.integer :price
       t.references :category, foreign_key: true
 
       t.timestamps
