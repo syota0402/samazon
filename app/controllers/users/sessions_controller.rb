3fce77858580c1eb00b731e943aab094cd70e408
syota0402 <syota.3y5s@gmail.com>
2021-08-04 13:23:20 +0000

    消し忘れ

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,7 +20,6 @@ class Users::SessionsController < Devise::SessionsController
   
   def after_sign_in_path_for(user)
     products_path
-    binding.pry
   end
   
   def after_sign_out_path_for(user)
