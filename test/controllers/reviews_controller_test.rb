3270f0cf705b11d93905b4c2f476b3eb3fa40745
syota0402 <87374457+syota0402@users.noreply.github.com>
2021-08-09 16:06:34 +0900

    Revert "9章　レビューを投稿できるようにする"

--- test/controllers/reviews_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ReviewsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get reviews_create_url
-    assert_response :success
-  end
-
-end
