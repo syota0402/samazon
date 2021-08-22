76ff099e0ca2ba2cb321dd7f82c8a24491c04545
syota0402 <syota.3y5s@gmail.com>
2021-08-22 09:05:29 +0000

    ログイン後のリダイレクト先を変更

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -17,6 +17,14 @@ class Admins::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+  
+  def after_sign_in_path_for(user)
+    dashboard_path
+  end 
+  
+  def after_sign_out_path_for(user)
+    root_path
+  end
 
   protected
 
