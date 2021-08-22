86c74729df36ea8351fe5bf87aee649597b8790c
syota0402 <syota.3y5s@gmail.com>
2021-08-22 07:51:22 +0000

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  get "dashboard", :to => "dashboard#index"
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
+  
+  devise_scope :admins do 
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    get "dashboard/login", :to => "admins/sessions#destroy"
+  end
   
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
