963412248111aaec9da7d3b948222f3b8aca92af
syota0402 <syota.3y5s@gmail.com>
2021-09-16 14:01:49 +0000

    クレジットカード機能の実装

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -26,6 +26,14 @@ class ShoppingCartsController < ApplicationController
   def destroy
     @user_cart.buy_flag = true
     @user_cart.save
+    
+    Payjp.api_key = ENV["PATJP_SECRET_KEY"]
+    Payjp::Charge.create(
+                          :customer => current_user.token,
+                          :amount => @user_cart.total.to_i,
+                          :currency => 'jpy'
+                        )
+    
     redirect_to cart_users_url
   end
   
